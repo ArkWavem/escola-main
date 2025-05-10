c40b52547e5040a7ae2041e913b64f4ce9b400d0
ArkWavem <adfelix.oficial@gmail.com>
2025-05-10 12:11:05 -0300

    run: 1

---
+++ src/main/java/com/senai/escola/Controller/AlunoController.java
@@ -0,0 +1,50 @@
+package com.senai.escola.Controller;
+
+import com.senai.escola.Service.AlunoService;
+import com.senai.escola.model.Aluno;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService){
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public List<Aluno> listaDeAluno() {
+        return alunoService.buscarAlunos();
+    }
+
+    @GetMapping("/id")
+    public Aluno buscarAluno(@PathVariable Long id) {
+        return alunoService.buscarAlunoId(id);
+    }
+
+    @PostMapping("/{id}")
+    public Aluno criarAluno(@RequestBody Aluno aluno) {
+        return alunoService.salvaAluno(aluno);
+    }
+
+    @PutMapping("/{id}")
+    public Aluno atualizarAluno(@PathVariable Long id, @RequestBody Aluno alunoAlterado) {
+
+        Aluno verficaAluno = alunoService.buscarAlunoId(id);
+        if (verficaAluno ==  null) return null;
+        verficaAluno.setNomeAluno(alunoAlterado.getNomeAluno());
+        verficaAluno.setEmailAluno(alunoAlterado.getEmailAluno());
+        verficaAluno.setTelefoneAluno(alunoAlterado.getTelefoneAluno());
+        return alunoService.salvaAluno(verficaAluno);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarAluno(@PathVariable Long id){
+        alunoService.excluirAluno(id);
+    }
+
+}
---
+++ src/main/java/com/senai/escola/Service/AlunoService.java
@@ -0,0 +1,33 @@
+package com.senai.escola.Service;
+
+import com.senai.escola.Interface.AlunoRepository;
+import com.senai.escola.model.Aluno;
+
+import java.util.List;
+
+public class AlunoService {
+
+    private final AlunoRepository _repository;
+
+    public AlunoService(AlunoRepository repository) {
+        this._repository = repository;
+    }
+
+    public List<Aluno> buscarAlunos(){
+        return _repository.findAll();
+    }
+
+    public Aluno salvaAluno(Aluno aluno){
+        return _repository.save(aluno);
+    }
+
+    public void excluirAluno(Long id){
+        _repository.deleteById(id);
+    }
+
+    public Aluno buscarAlunoId(Long id){
+        return _repository.findById(id).orElse(null);
+    }
+
+
+}
